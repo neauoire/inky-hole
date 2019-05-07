d7186b3845ae3ef3ac4bbf4d500add3b1097c045
Devine Lu Linvega <aliceffekt@gmail.com>
2019-05-07 13:34:23 +0900

    Added ratio to display

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ try:
     json_string = f.read()
     parsed_json = json.loads(json_string)
     adsblocked = parsed_json['ads_blocked_today']
+    ratioblocked = parsed_json['ads_percentage_today']
     f.close()
 except:
     queries = '?'
@@ -27,7 +28,8 @@ font = ImageFont.truetype(FredokaOne, 32)
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
 
-draw.text((20,30), str(adsblocked), inky_display.BLACK, font)
+draw.text((20,20), str(adsblocked), inky_display.BLACK, font)
+draw.text((20,40), str("%.2f" % round(ratioblocked,2)) + "%", inky_display.BLACK, font)
 
 inky_display.set_image(img)
 
